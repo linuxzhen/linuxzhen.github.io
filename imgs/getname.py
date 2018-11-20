1d0ebca0e9ba062656c26fbbf00b0e38777af826
vaster <linuxzhen520@163.com>
2018-11-20 15:04:11 +0800

    'xxx'

--- imgs/getname.py
+++ imgs/getname.py
@@ -5,6 +5,6 @@
 
 import os
 gitimg_url = "https://linuxzhen.github.io/imgs/"
-with open("img_list.txt", 'rw', encoding='utf-8') as fp
-    for file in os.listfile():
-        fp.write(gitimg_url+file)
+with open("img_list.txt", "w+", encoding='utf-8') as fp:
+    for file in os.listdir():
+        fp.write(gitimg_url+file+"\n")
